ac9d4d8cb67c4842e488833477cfa5a5bcac82a7
Yury Voloshin <yvoloshin01@gmail.com>
2015-06-16 05:08:07 +0000

    depth-first green 2

--- tree_depth.rb
+++ tree_depth.rb
@@ -13,30 +13,37 @@ end
 def depth_first(node, target_value)
 	@trunk = node
 	@branch_idx = 0
-	@pass = 0
 	@branch_arr = []
-	depth_first_continued(node, target_value)
+	@branch_arr_idx = 0
+	depth_first_recursive(node, target_value)
 end
 
-def depth_first_continued(node, target_value)
+def depth_first_recursive(node, target_value)
   return true if node.payload == target_value
   current_node = node
   puts "payload=#{current_node.payload}"
   if current_node.children[@branch_idx+1] != nil
   	@branch_arr.unshift(current_node)
-  	puts @branch_arr.inspect
+  #	puts @branch_arr.inspect
   end
   if current_node.children.empty?
-  	branch_node = @branch_arr.shift
-  	puts "branch_node ="
-  	puts branch_node.inspect
-  	current_node = branch_node.children[1]
-  	#current_node = current_node.children[1]
-  	puts "current_node ="
-  	puts current_node.inspect
+  	branch_node = @branch_arr[@branch_arr_idx]
+  	@branch_idx += 1 
+  	if (@branch_idx >= branch_node.children.length) && (@branch_arr_idx >= @branch_arr.length-1)
+  		return false
+  	elsif @branch_idx >= branch_node.children.length
+  		@branch_arr_idx += 1
+  		branch_node = @branch_arr[@branch_arr_idx]
+  		@branch_idx = 1
+  	end
+  #	puts "branch_node ="
+  #	puts branch_node.inspect
+  	current_node = branch_node.children[@branch_idx]
+  #	puts "current_node ="
+  #	puts current_node.inspect
   	else current_node = current_node.children[0]
   end
-  depth_first_continued(current_node, target_value)
+  depth_first_recursive(current_node, target_value)
 end
 
 # The "Leafs" of a tree, elements that have no children
@@ -53,6 +60,6 @@ fifth_node = Tree.new(5, [ninth_node])
 
 # The "Trunk" of the tree
 trunk   = Tree.new(2, [seventh_node, fifth_node])
-puts depth_first(trunk, 11).to_s
+puts depth_first(trunk, 10).to_s
 
 
